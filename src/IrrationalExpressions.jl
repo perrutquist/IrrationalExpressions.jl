4e8f8f603f983266fcdeb7e18be0e234104c7b15
Per Rutquist <per.rutquist@gmail.com>
2017-03-06 17:49:55 +0100

    show method

--- src/IrrationalExpressions.jl
+++ src/IrrationalExpressions.jl
@@ -12,9 +12,9 @@ rather than to approximately 1.2e-16.
 
 module IrrationalExpressions
 
-import Base: +, -, *, /, convert, promote_rule
+import Base: +, -, *, /, convert, promote_rule, show
 
-immutable IrrationalExpr{op, TU<:Tuple} <: Real
+immutable IrrationalExpr{op, TU<:Tuple} <: Number
   args::TU
 end
 
@@ -30,11 +30,22 @@ promote_rule{T1<:AbstractFloat, T2<:IrrationalExpr}(::Type{T1}, ::Type{T2}) = T1
 
 ## Binary operators
 ops = (:(+), :(-), :(*), :(/))
-types = (Irrational, IrrationalExpr, Integer, Rational)
+types = (IrrationalExpr, Irrational, Rational, Integer, Bool)
 for op in ops, i in eachindex(types), j in eachindex(types)
   if i<=2 || j<=2
     @eval $op(x::$(types[i]), y::$(types[j])) = IrrationalExpr{Symbol($op),Tuple{$(types[i]),$(types[j])}}((x,y))
   end
 end
+
+# Utility function for converting to Expr
+getsym(x) = x
+getsym{sym}(::Irrational{sym}) = sym
+
+# We define conversion to Expr as a conveninent way to get strings,
+# but this also alows eval(convert(Expr, x)) as a roundabout way to get x.
+convert{op,TU}(::Type{Expr}, x::IrrationalExpr{op,TU}) =
+  Expr(:call, op, map(getsym,x.args)...)
+
+show(io::IO, x::IrrationalExpr) = print(io, string(convert(Expr, x)), " ≈ ", Float64(x))
 
 end # module
