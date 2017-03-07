655b170eefed4a0dd0e5afb2f94439f904f51e0b
Per Rutquist <per.rutquist@gmail.com>
2017-03-07 10:32:06 +0100

    N as type parameter

--- src/IrrationalExpressions.jl
+++ src/IrrationalExpressions.jl
@@ -14,35 +14,35 @@ module IrrationalExpressions
 
 import Base: +, -, *, /, convert, promote_rule, show
 
-immutable IrrationalExpr{op, TU<:Tuple} <: Real
-  args::TU
+immutable IrrationalExpr{op, N} <: Real
+  args::NTuple{N,Union{IrrationalExpr, Irrational, Rational, Integer}}
 end
 
-@generated convert{T<:AbstractFloat,op,TU}(::Type{T}, x::IrrationalExpr{op,TU}) =
-  Expr(:call, op, [Expr(:call, :convert, T, :( x.args[$i] )) for i=1:length(TU.parameters)]...)
+@generated convert{T<:AbstractFloat,op,N}(::Type{T}, x::IrrationalExpr{op,N}) =
+  Expr(:call, op, [Expr(:call, :convert, T, :( x.args[$i] )) for i=1:N]...)
 
 promote_rule{T1<:AbstractFloat, T2<:IrrationalExpr}(::Type{T1}, ::Type{T2}) = T1
 promote_rule{T2<:IrrationalExpr}(::Type{BigFloat}, ::Type{T2}) = BigFloat
 
 ## Unary operators
 (+)(x::IrrationalExpr) = x
-(-)(x::IrrationalExpr) = IrrationalExpr{:(-),Tuple{IrrationalExpr}}((x,))
-(-)(x::Irrational) = IrrationalExpr{:(-),Tuple{Irrational}}((x,))
+(-)(x::IrrationalExpr) = IrrationalExpr{:(-),1}((x,))
+(-)(x::Irrational) = IrrationalExpr{:(-),1}((x,))
 
 ## Binary operators
 ops = (:(+), :(-), :(*), :(/))
-types = (IrrationalExpr, Irrational, Rational, Integer, Bool)
+types = (IrrationalExpr, Irrational, Rational, Integer)
 for op in ops, i in eachindex(types), j in eachindex(types)
   if i<=2 || j<=2
-    @eval $op(x::$(types[i]), y::$(types[j])) = IrrationalExpr{Symbol($op),Tuple{$(types[i]),$(types[j])}}((x,y))
+    @eval $op(x::$(types[i]), y::$(types[j])) = IrrationalExpr{Symbol($op),2}((x,y))
   end
 end
 
 # We define getexpr() as a conveninent middle step to get to strings,
-# but this also alows eval(getexpr(x)) as a roundabout way to get x.
+# but this also allows eval(getexpr(x)) as a roundabout way to get x.
 getexpr(x) = x
 getexpr{sym}(::Irrational{sym}) = sym
-getexpr{op,TU}(x::IrrationalExpr{op,TU}) = Expr(:call, op, map(getexpr,x.args)...)
+getexpr{op,N}(x::IrrationalExpr{op,N}) = Expr(:call, op, map(getexpr,x.args)...)
 
 show(io::IO, x::IrrationalExpr) = print(io, string(getexpr(x)), " = ", string(Float64(x))[1:end-2], "...")
 
